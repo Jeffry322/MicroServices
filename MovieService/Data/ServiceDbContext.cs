55abadb79ac89dc699ea3b9b1aa7317cb308fe16
Jeffry322 <67588481+Jeffry322@users.noreply.github.com>
2024-07-27 18:05:51 +0300

    add db context and first model

---
+++ MovieService/Data/ServiceDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MovieService.Data
+{
+    public sealed class ServiceDbContext : DbContext
+    {
+        public ServiceDbContext(DbContextOptions<ServiceDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
